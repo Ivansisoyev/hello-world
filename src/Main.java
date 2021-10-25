2405fab706f296f10bc434e7dceb32b533149770
ivan <ivan08021991@gmail.com>
2021-10-25 15:13:08 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+ Scanner scanner = new Scanner (System.in);
+ System.out.println("Настя дура");
+ String phrase1 = scanner.nextLine();
+ System.out.println(phrase1);
+
+
+
+
+
+
+    }
+
+
+}
